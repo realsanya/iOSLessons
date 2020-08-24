c7901397899229d6f7564860388db6be6656637a
Morozova Sasha <sashamorozova01@mail.ru>
2020-08-24 11:41:48 +0300

    main storyboard

---
+++ CustomAlert/CustomAlert/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CustomAlert
+//
+//  Created by Student 6 on 24/08/2020.
+//  Copyright © 2020 Student 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lightAlertButton: UIButton!
+    @IBOutlet var darkAlertButton: UIButton!
+    @IBOutlet var alertWithThreeButtons: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
