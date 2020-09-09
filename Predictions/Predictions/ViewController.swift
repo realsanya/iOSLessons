16815a519b684b4fa9b7f196712cf3700a52da9d
Morozova Sasha <sashamorozova01@mail.ru>
2020-09-09 14:09:38 +0300

    watchKit app

---
+++ Predictions/Predictions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Predictions
+//
+//  Created by Student 6 on 09/09/2020.
+//  Copyright © 2020 Student 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
